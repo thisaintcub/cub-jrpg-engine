7ed9777bfba4700605c02b43e894d01903c102fe
cub <dolpshyosu@gmail.com>
2025-01-19 00:10:05 -0700

    yea

--- obj/Debug/net8.0/cub jrpg engine.AssemblyInfo.cs
+++ obj/Debug/net8.0/cub jrpg engine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cub jrpg engine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ea8a5e4efa6434913312e544f896be922e47af8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f95592f3aaf91948bc4516d077cdb86e2fde2f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("cub jrpg engine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cub jrpg engine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
